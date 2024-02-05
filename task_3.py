c5f39ed88eef3ebc63c08100c0f9fe8bb18d8f1e
ilyabaykov <i-baykov@internet.ru>
2024-02-05 15:36:26 +0300

    Добавил описание задания и теперь словарь формируется через генератор словарей

--- task_3.py
+++ task_3.py
@@ -1,12 +1,14 @@
 from task_1 import Connect
 
+"""
+    1) Необходимо просмотреть все товары
+    и найти сумму всех позиций ( количество товаров  * на цену товара )
+    
+"""
+
 total_sum = 0
 URL = 'https://parsinger.ru/html/index1_page_1.html'
 
 connect = Connect(URL)
 soup = connect.get_soup()
-nav_menu = {}
-for link in soup.find('div', class_='nav_menu').find_all('a'):
-    nav_menu[link.text] = link['href']
-
-
+nav_menu = {link.text: link['href'] for link in soup.find('div', class_='nav_menu').find_all('a')}
