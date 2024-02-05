4fbf2ccf00d636935a51996d3cd2a535fdff10a8
ilyabaykov <i-baykov@internet.ru>
2024-02-06 01:47:46 +0300

    Получил по-строчно все элементы таблицы

--- task_7.py
+++ task_7.py
@@ -1,15 +1,17 @@
 from task_1 import Connect
 
-"""
-    Умножить число в оранжевой ячейке на число в голубой ячейке в той же строке
-    и всё суммировать
-    
-    * усложнение для себя : если в строке несколько оранжевых ячеек , то необходимо число из каждой такой ячейки 
-    умножить на число из голубой ячейки и сложить полученные результаты
-"""
 url = 'https://parsinger.ru/table/5/index.html'
 connect = Connect(url)
 soup = connect.get_soup()
+
+"""
+    TASK_001:
+    Умножить число в оранжевой ячейке на число в голубой ячейке в той же строке
+    и всё суммировать
+
+    * усложнение для себя : если в строке несколько оранжевых ячеек , то необходимо число из каждой такой ячейки 
+    умножить на число из голубой ячейки и сложить полученные результаты
+"""
 total_summa = 0
 for count_line, tr in enumerate(soup.find_all('tr')[1:], start=1):
     orange_cells = []
@@ -20,4 +22,20 @@ for count_line, tr in enumerate(soup.find_all('tr')[1:], start=1):
         if count_td == 15:
             blue_cells = float(td.text)
     total_summa += round(sum([digit * blue_cells for digit in orange_cells]), 3)
-print(round(total_summa, 3))
+# print(round(total_summa, 3))
+
+"""
+    TASK_002:
+    Сформировать словарь,где ключ будет автоматически формироваться из заголовка столбцов,
+    а значения это сумма всех чисел в столбце;
+    Пример: {'1 column' : 000.000, '2 column' : 000.000, ..., '15 column' : 000.000,}
+    
+"""
+
+for count, tr in enumerate(soup.find_all("tr")):
+    if count == 0:
+        print(tr.find_all('th'))
+    else:
+        current_td = tr.find_all('td')
+        print(current_td)
+    print()
