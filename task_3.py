80c2a1ba11d0002f3d150ddfc21f58c8d85e3470
ilyabaykov <i-baykov@internet.ru>
2024-02-05 15:45:28 +0300

    Получил словарь всех страниц пагинаций

--- task_3.py
+++ task_3.py
@@ -12,3 +12,6 @@ URL = 'https://parsinger.ru/html/index1_page_1.html'
 connect = Connect(URL)
 soup = connect.get_soup()
 nav_menu = {link.text: link['href'] for link in soup.find('div', class_='nav_menu').find_all('a')}
+pagen_menu = {link.text: link['href'] for link in connect.get_soup().find('div', class_='pagen').find_all('a')}
+print(nav_menu)
+print(pagen_menu)
