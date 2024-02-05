5eb96e6c6a1c7752349937158741cdf8cbd9e313
ilyabaykov <i-baykov@internet.ru>
2024-02-05 16:08:56 +0300

    Получил ссылкки на ВСЕ ТОВАРЫ

--- task_3.py
+++ task_3.py
@@ -14,9 +14,13 @@ soup = connect.get_soup()
 nav_menu = {link.text: link['href'] for link in soup.find('div', class_='nav_menu').find_all('a')}
 pagen_menu = {link.text: link['href'] for link in connect.get_soup().find('div', class_='pagen').find_all('a')}
 
-
 all_links = []
 for category in nav_menu.values():
     all_links.extend(link['href'] for link in
                      Connect(f"https://parsinger.ru/html/{category}").get_soup().find('div', class_='pagen').find_all(
                          'a'))
+all_goods = []
+for link in all_links:
+    all_goods.extend([f"https://parsinger.ru/html/{item.find('a')['href']}" for item in
+                      Connect(f"https://parsinger.ru/html/{link}").get_soup().find_all('div', 'item')])
+
