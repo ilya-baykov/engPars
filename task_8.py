bb1a7e020229e3bd6409993560a3e1ae41b968e9
ilyabaykov <i-baykov@internet.ru>
2024-02-09 02:12:54 +0300

    Записал информацию по каждой мышке в csv-файл

--- task_8.py
+++ task_8.py
@@ -1,13 +1,17 @@
 import csv
 from task_1 import Connect
 
+with open('products.csv', 'w', newline='', encoding='utf-8-sig') as csvfile:
+    writer = csv.writer(csvfile, delimiter=';')
+    writer.writerow(['Наименованиe', 'Цена', 'Бренд', 'Тип', 'Подключение', 'Игровая(да/нет)'])
+
 URL = 'https://parsinger.ru/html/index3_page_1.html'
 connect = Connect(URL)
 soup = connect.get_soup()
 
 pagen_links = [f"https://parsinger.ru/html/{link['href']}" for link in soup.find('div', class_='pagen').find_all('a')]
 
-all_product = []
+all_product = {}
 for link in pagen_links:
     items_on_page = Connect(url=link).get_soup().find_all('div', 'item')
     for item in items_on_page:
@@ -15,3 +19,9 @@ for link in pagen_links:
         price = item.find('p', class_='price').text
         brand, type_mouse, connection, is_gamer = [info.text.split(':')[1] for info in
                                                    item.find('div', class_='description').find_all('li')]
+        all_product[name] = {'price': price, 'brand': brand, 'type': type_mouse, 'connection': connection,
+                             'is_gamer': is_gamer}
+with open('products.csv', 'a', newline='', encoding='utf-8-sig') as csvfile:
+    writer = csv.writer(csvfile, delimiter=';')
+    for key, value in all_product.items():
+        writer.writerow([key, *value.values()])
